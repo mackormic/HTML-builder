2bd21a5082095ca83c0075e6a1f26cf77719a2d5
kenny <k.mackormic@mail.ru>
2022-11-08 23:55:59 +0300

    fix: change 01-read-file jn ReadStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,19 @@
+// const path = require("path");
+// const fs = require("fs");
+// const { stdout } = require("process");
+
+// fs.readFile(path.join(__dirname, "text.txt"), "utf-8", (err, data) => {
+//   if (err) throw err;
+//   //   console.log(data);
+//   stdout.write(data);
+//   process.exit();
+// });
 const path = require("path");
 const fs = require("fs");
-const { stdout } = require("process");
-
-fs.readFile(path.join(__dirname, "text.txt"), "utf-8", (err, data) => {
-  if (err) throw err;
-  //   console.log(data);
-  stdout.write(data);
-  process.exit();
+const stream = new fs.ReadStream(path.join(__dirname, "text.txt"), {
+  encoding: "utf-8",
+});
+stream.on("readable", function () {
+  const data = stream.read();
+  if (data != null) console.log(data);
 });
